cdf23bbad286109c138b4b8f6eca0f1eccf31b93
bonellia <taha.yasin@kril.me>
2019-07-22 17:24:38 +0300

    Add main page layout/java

--- app/src/main/java/com/example/macrorecapp/MainPage.java
+++ app/src/main/java/com/example/macrorecapp/MainPage.java
@@ -3,6 +3,7 @@ package com.example.macrorecapp;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends Activity {
 
@@ -10,5 +11,20 @@ public class MainPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void openLiveControl(View view) {
+    }
+
+    public void openMotionControl(View view) {
+    }
+
+    public void openTimeLapse(View view) {
+    }
+
+    public void openSettings(View view) {
+    }
+
+    public void openMyCloud(View view) {
     }
 }
