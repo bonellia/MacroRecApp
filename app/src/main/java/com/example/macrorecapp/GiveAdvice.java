5b50eca31a5b3eebd4ba196fdad7d6414a52f383
bonellia <taha.yasin@kril.me>
2019-07-30 18:20:18 +0300

    Add coupon reward dialog for Give Advice layout.

--- app/src/main/java/com/example/macrorecapp/GiveAdvice.java
+++ app/src/main/java/com/example/macrorecapp/GiveAdvice.java
@@ -1,11 +1,18 @@
 package com.example.macrorecapp;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class GiveAdvice extends Activity {
+
+    final Context context = this;
+    private Button button;
 
     Spinner spinner;
     String defaultTextForSpinner = "Select Category";
@@ -17,6 +24,30 @@ public class GiveAdvice extends Activity {
         setContentView(R.layout.activity_give_advice);
         spinner = findViewById(R.id.spinnerCategory);
         spinner.setAdapter(new CustomSpinnerAdapter(this, R.layout.spinner_layout, arrayForSpinner, defaultTextForSpinner));
+
+        button = findViewById(R.id.send_button);
+
+        // Add activity layout listener for "Change Password" Button.
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Set custom dialog view style.
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_give_advice);
+
+                Button dialogButton = dialog.findViewById(R.id.dialog_button_ok);
+                // Set dialog layout listener for "Okay" Button.
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
     }
 
     public void goBack(View view) {
